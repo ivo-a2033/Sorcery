8e7dce709d5ab39ad9ba1fd7cf73354a5dbea3ee
ivo-a2033 <ivoeputchi234@gmail.com>
2024-07-16 23:02:25 +0100

    small changes 2

--- Player.gd
+++ Player.gd
@@ -60,6 +60,7 @@ var symbols = {
 	1: load("res://one_symbol.png")
 }
 
+var label_settings = load("res://new_label_settings.tres")
 
 func _ready():
 	if Globals.p1 == null:
@@ -272,6 +273,7 @@ func spawn_dog(pos):
 func take_dmg(dmg):
 	hp -= dmg
 	var label = Label.new()
+	label.label_settings = label_settings
 	label.text = "-" + str(dmg)
 	label.position = position + Vector2(Globals.rng.randf_range(0,64), 0).rotated(Globals.rng.randf_range(0, PI * 2))
 	get_parent().add_child(label)
